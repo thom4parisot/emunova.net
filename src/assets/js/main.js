4d4d3757d68344f5c02256e8190693bd75c5d597
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-09-24 19:13:13 +0100

    Toggling accordion

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,16 +9,25 @@
       this.container = container;
     };
 
+    function getMegamenu(el){
+      return document.querySelector(".megamenu-"+el.getAttribute("data-target"));
+    }
+
     Accordion.prototype = {
       activate: function activateElement(el){
-
+        el.classList.remove("collapsed");
+        getMegamenu(el).classList.add("in");
+      },
+      deactivate: function deactivateElement(el){
+        el.classList.add("collapsed");
+        getMegamenu(el).classList.remove("in");
       },
       deactivateAll: function deactivateAllElement(){
-
+        [].slice.call(this.container.querySelectorAll(".accordion-toggle")).forEach(this.deactivate);
       },
       handleClick: function handleClick(el){
-        el.classList.remove("collapsed");
-        document.querySelector(".megamenu-"+el.getAttribute("data-target")).classList.add("in");
+        this.deactivateAll();
+        this.activate(el);
       },
       registerEvents: function registerEvents(){
         var self = this;
