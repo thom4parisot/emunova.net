88c0f7d95bbd0f6a6c2f0f87f64421b98f804cd8
oncletom <hi@oncletom.io>
2016-05-01 14:30:16 +0100

    feat(search): ui and fix indexed urls

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,7 +9,8 @@ const lzld = require('lazyloadjs');
 const DynamicSorter = require('./dynamic-sorter');
 const RemoteContent = require('./rss/remote-content');
 const SortTable = require('sorttable');
-const instantsearch = require('instantsearch.js');
+const searchClient = require('algoliasearch');
+const debounce = require('lodash/debounce');
 const stars = require('../../../lib/handlebars/ratings').stars;
 
 $('.lazyload img').each((i, el) => lzld(el));
@@ -31,53 +32,59 @@ $menus.on('show.bs.collapse',() => {
   });
 });
 
-const search = instantsearch({
-  appId: 'C5SU6DIDX6',
-  apiKey: 'f2a531624b466c569e171f233c98ea3d',
-  indexName: 'prod_games',
-  protocol: 'https'
-});
+const index = searchClient('C5SU6DIDX6', 'f2a531624b466c569e171f233c98ea3d', {
+  protocol: 'https:'
+}).initIndex('prod_games');
 
-search.addWidget(
-  instantsearch.widgets.searchBox({
-    container: '#topbar-search-input',
-    autofocus: false,
-    queryHook: (query, search) => {
-      if (query && !$collapsibleSearch.hasClass('in')) {
-	$collapsibleSearch.collapse('show');
+$('#topbar-search-input').on('keyup', debounce(e => handleSearch(e.currentTarget.value), 150))
+
+function handleSearch (query) {
+  const $collapsibleSearch = $('#search-results-wrapper .collapse')
+  const searchParams = {
+    hitsPerPage: 12
+  };
+
+  index.search(query, searchParams)
+    .then((response) => {
+      const itemsResults = response.hits.map(item => (
+        `<div class="col-lg-4 col-sm-6">
+          <a class="result-link" href="/${item.url}">
+          	<span class="result-title">${item._highlightResult.title.value}</span>
+
+          	<ul class="result-metadata">
+          	  <li class="result-metadata__item result-metadata__item--system">${item.system.name}</li>
+          	  <li class="result-metadata__item result-metadata__item--date">${item.released}</li>
+          	  <li class="result-metadata__item result-metadata__item--rating">${stars(item.rating)}</li>
+          	</ul>
+          </a>
+        </div>`
+      ));
+
+      $('#search-results-wrapper .panel-body').html(itemsResults)
+      return response.hits;
+    })
+    .then((hits) => {
+      if (query && hits.length && !$collapsibleSearch.hasClass('in')) {
+         $collapsibleSearch.collapse('show');
       }
-      else if (!query && $collapsibleSearch.hasClass('in')) {
-	$collapsibleSearch.collapse('hide');
+      else if ((!query || !hits.length) && $collapsibleSearch.hasClass('in')) {
+         $collapsibleSearch.collapse('hide');
       }
+    });
+}
 
-      search(query);
-    }
-  })
-);
-
-const $collapsibleSearch = $('#search-results-wrapper .collapse')
-
-search.addWidget(
-  instantsearch.widgets.hits({
-    container: '#search-results-wrapper .panel-body',
-    hitsPerPage: 12,
-    templates: {
-      empty: 'Aucun résultat trouvé :-(',
-      item: (item) => `<a class="result-link" href="#">
-	<span class="result-title">${item._highlightResult.title.value}</span>
-
-	<ul>
-	  <li>Super NES</li>
-	  <li>${item.released}</li>
-	  <li>${stars(item.rating)}</li>
-	</ul>
-      </a>`,
-    },
-    cssClasses: {
-      root: ['row'],
-      item: ['col-lg-4', 'col-sm-6']
-    }
-  })
-);
-
-search.start();
+//
+// search.addWidget(
+//   instantsearch.widgets.hits({
+//     container: '#search-results-wrapper .panel-body',
+//     hitsPerPage: 12,
+//     templates: {
+//       empty: 'Aucun résultat trouvé :-(',
+//       item: (item) => ,
+//     },
+//     cssClasses: {
+//       root: ['row'],
+//       item: ['col-lg-4', 'col-sm-6']
+//     }
+//   })
+// );
