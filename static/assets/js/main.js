0322c6fd20d340906919a907ed9408a767bb7754
Thomas Parisot <hi@oncletom.io>
2017-11-08 09:32:31 +0000

    Continuous JS/CSS build

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,20 +1,18 @@
 'use strict';
 
-const $ = global.jQuery = require('jquery');
+const $ = (global.jQuery = require('jquery'));
 require('bootstrap/transition');
 require('bootstrap/collapse');
 require('bootstrap/dropdown');
 
-const lzld = require('lazyloadjs');
 const DynamicSorter = require('./dynamic-sorter');
 const RemoteContent = require('./rss/remote-content');
-const SortTable = require('sorttable');
+// const SortTable = require('sorttable');
 const searchClient = require('algoliasearch/lite');
 const debounce = require('lodash/debounce');
-const stars = require('../../../lib/handlebars/ratings').stars;
+const { generateStars: stars } = require('./ratings');
 
-$('.lazyload img').each((i, el) => lzld(el));
-$('table.table-sortable').each((i, el) => SortTable.makeSortable(el));
+// $('table.table-sortable').each((i, el) => SortTable.makeSortable(el));
 $('form[data-target]').each((i, el) => {
   new DynamicSorter(el).registerEvents();
 });
@@ -24,7 +22,7 @@ $('.rss-content').each((i, el) => {
 
 const $menus = $('#megamenu-system, #megamenu-game');
 
-$menus.on('show.bs.collapse',() => {
+$menus.on('show.bs.collapse', () => {
   const $others = $menus.not(this);
 
   $others.collapse('hide').each((i, el) => {
@@ -36,39 +34,50 @@ const index = searchClient('C5SU6DIDX6', 'f2a531624b466c569e171f233c98ea3d', {
   protocol: 'https:'
 }).initIndex('prod_games');
 
-$('#topbar-search-input').on('keyup', debounce(e => handleSearch(e.currentTarget.value), 150))
+$('#topbar-search-input').on(
+  'keyup',
+  debounce(e => handleSearch(e.currentTarget.value), 150)
+);
 
-function handleSearch (query) {
-  const $collapsibleSearch = $('#search-results-wrapper .collapse')
+function handleSearch(query) {
+  const $collapsibleSearch = $('#search-results-wrapper .collapse');
   const searchParams = {
     hitsPerPage: 12
   };
 
-  index.search(query, searchParams)
-    .then((response) => {
-      const itemsResults = response.hits.map(item => (
-        `<div class="col-lg-4 col-sm-6">
+  index
+    .search(query, searchParams)
+    .then(response => {
+      const itemsResults = response.hits.map(
+        item =>
+          `<div class="col-lg-4 col-sm-6">
           <a class="result-link" href="/${item.url}">
-          	<span class="result-title">${item._highlightResult.title.value}</span>
+          	<span class="result-title">${item._highlightResult.title
+              .value}</span>
 
           	<ul class="result-metadata">
-          	  <li class="result-metadata__item result-metadata__item--system">${item.system.name}</li>
+          	  <li class="result-metadata__item result-metadata__item--system">${item
+                .system.name}</li>
           	  <li class="result-metadata__item result-metadata__item--date">${item.released}</li>
-          	  <li class="result-metadata__item result-metadata__item--rating">${stars(item.rating)}</li>
+          	  <li class="result-metadata__item result-metadata__item--rating">${stars(
+                item.rating
+              )}</li>
           	</ul>
           </a>
         </div>`
-      ));
+      );
 
-      $('#search-results-wrapper .panel-body').html(itemsResults)
+      $('#search-results-wrapper .panel-body').html(itemsResults);
       return response.hits;
     })
-    .then((hits) => {
+    .then(hits => {
       if (query && hits.length && !$collapsibleSearch.hasClass('in')) {
-         $collapsibleSearch.collapse('show');
-      }
-      else if ((!query || !hits.length) && $collapsibleSearch.hasClass('in')) {
-         $collapsibleSearch.collapse('hide');
+        $collapsibleSearch.collapse('show');
+      } else if (
+        (!query || !hits.length) &&
+        $collapsibleSearch.hasClass('in')
+      ) {
+        $collapsibleSearch.collapse('hide');
       }
     });
 }
